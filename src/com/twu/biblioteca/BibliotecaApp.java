f4b96c874d984909a2280ae77f70550ceb3cd6af
Marcio Vinicius Campos da Silveira <lamarciovinicius.s@MacBook-Pro-de-Marcio.local>
2020-01-28 15:04:26 -0300

    1.1 View a welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -4,5 +4,18 @@ public class BibliotecaApp {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+
+        String message = bibliotecaApp.welcomeMessage();
+        System.out.println(message);
     }
+
+    public String welcomeMessage() {
+        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    }
+
 }
+
+
+
+
---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,29 @@
+
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BibliotecaAppTest {
+
+    private BibliotecaApp bibliotecaApp;
+
+    @Before
+    public void setUp() throws Exception {
+        bibliotecaApp = new BibliotecaApp();
+    }
+
+    @Test
+
+    public void shouldShowWelcomeMessage() {
+
+        String message = bibliotecaApp.welcomeMessage();
+
+        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!", message);
+
+    }
+
+} 
